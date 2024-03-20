e23f0f92170b9e2ef75141f4f965e0b4b1bf983c
Roberto Rodrigues a5278 <a5278@alunos.ipca.pt>
2024-03-20 19:24:21 +0000

    templates e sites html

--- venv/techaconchego/alojamento/views.py
+++ venv/techaconchego/alojamento/views.py
@@ -7,9 +7,17 @@ from django.http import HttpResponse
 # criar as views da app alojamento, para serem chamadas
 # no main TechAconchego
 
-def home(request):
-    return HttpResponse('<h1>TechAconchego Alojamento Home</h1>')
+## usando httpresponse
+## def home(request):
+   ## return HttpResponse('<h1>TechAconchego Alojamento Home</h1>')
 
+## def about(request):
+    ## return HttpResponse('<h1>TechAconchego Alojamento About</h1>')
+
+# usando render
+
+def home(request):
+    return render(request, 'alojamento/home.html', {'title': 'TechAconchego Alojamento Home'})
 
 def about(request):
-    return HttpResponse('<h1>TechAconchego Alojamento About</h1>')
+    return render(request, 'alojamento/about.html', {'title': 'TechAconchego Alojamento About'})
--- venv/techaconchego/techaconchego/settings.py
+++ venv/techaconchego/techaconchego/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'alojamento.apps.AlojamentoConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
