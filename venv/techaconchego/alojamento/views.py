c71c2a72eceef1e29c106b33c1afaf854e4f726e
Roberto Rodrigues a5278 <a5278@alunos.ipca.pt>
2024-03-20 18:43:46 +0000

    urls alojamento e main

--- venv/techaconchego/alojamento/views.py
+++ venv/techaconchego/alojamento/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# criar as views da app alojamento, para serem chamadas
+# no main TechAconchego
+
+def home(request):
+    return HttpResponse('<h1>TechAconchego Alojamento Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>TechAconchego Alojamento About</h1>')
